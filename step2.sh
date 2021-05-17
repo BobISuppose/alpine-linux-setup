92a2b9e7c820105b57e14804cda28a49090573ad
BobISuppose <82328576+BobISuppose@users.noreply.github.com>
2021-05-16 20:30:33 -0700

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,3 +1,5 @@
+#I am using this bash code on real hardware. If you need VMware tools go to https://github.com/ibuetler/alpine-linux-setup
+
 #!/bin/ash
 
 apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash termite zsh dbus dbus-x11
@@ -8,40 +10,38 @@ apk add mesa-gl glib feh firefox-esr accountsservice openvpn
 apk add docker docker-compose
 
 # add user
-adduser ibuetler
-mkdir -p /home/ibuetler/wallpaper
-mkdir -p /home/ibuetler/.config/i3
+adduser robert
+mkdir -p /home/robert/wallpaper
+mkdir -p /home/robert/.config/i3
 
-# user setup ibuetler
-cp ./ibuetler/wallpaper/compass.jpg /home/ibuetler/wallpaper/compass.jpg
-cp ./ibuetler/.config/i3/config /home/ibuetler/.config/i3/config
-cp ./ibuetler/.profile /home/ibuetler/.profile
-mkdir -p /home/ibuetler/.scripts
-cp ./ibuetler/login-script.sh /home/ibuetler/.scripts/login-script.sh
-chown -R ibuetler:ibuetler /home/ibuetler
+# user setup robert
+cp ./robert/wallpaper/compass.jpg /home/robert/wallpaper/compass.jpg
+cp ./robert/.config/i3/config /home/robert/.config/i3/config
+cp ./robert/.profile /home/robert/.profile
+mkdir -p /home/robert/.scripts
+cp ./robert/login-script.sh /home/robert/.scripts/login-script.sh
+chown -R robert:robert /home/robert
 
-# add ibuetler to sudoers
-cat ./ibuetler/sudoers >> /etc/sudoers
+# add robert to sudoers
+cat ./robert/sudoers >> /etc/sudoers
 
 # greeter background
-echo "background=/home/ibuetler/wallpaper/compass.jpg" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "background=/home/robert/wallpaper/compass.jpg" >> /etc/lightdm/lightdm-gtk-greeter.conf
 
 # set background image in accountsservice
-cp ./ibuetler/ibuetler /var/lib/AccountsService/users
-chown root:root /var/lib/AccountsService/users/ibuetler
+cp ./robert/robert /var/lib/AccountsService/users
+chown root:root /var/lib/AccountsService/users/robert
 
 # add user to docker
-addgroup ibuetler docker
+addgroup robert docker
 
-# enable copy paste in vmware
-chmod g+s /usr/bin/vmware-user-suid-wrapper
 
-# give ibuetler write access to /opt dir
-chown ibuetler:ibuetler /opt
+# give robert write access to /opt dir
+chown robert:robert /opt
 
 # mkdir /opt/docker
 mkdir -p /opt/docker
 cp ./docker/* /opt/docker/
-chown ibuetler:ibuetler /opt/docker
+chown robert:robert /opt/docker
 
 
