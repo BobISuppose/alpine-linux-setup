64cd089d83c7465243893b969202b39688234503
BobISuppose <82328576+BobISuppose@users.noreply.github.com>
2021-05-16 20:22:37 -0700

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -6,9 +6,6 @@ rc-update add dbus
 rc-service lightdm start
 rc-update add lightdm
 
-rc-service open-vm-tools start
-rc-update add open-vm-tools boot
-
 rc-service docker start
 rc-update add docker boot
 
